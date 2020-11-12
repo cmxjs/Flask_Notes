e346ebd2c2ac3212ea91fb6b9e05b58ef59dcb41
Xiao Jinshui <xiaojinshui@casachina.com.cn>
2020-11-12 11:31:34 +0800

    add Quickstart.md

--- hello.py
+++ hello.py
@@ -4,6 +4,9 @@
 @File Name: hello.py
 @Author: cmxjs
 @Description: A Simple Example, start cmd: env FLASK_APP=hello.py flask run
+              externally visible server: flask run -h 0.0.0.0
+              get more flask run option: flask run --help
+
 """
 from flask import Flask
 
